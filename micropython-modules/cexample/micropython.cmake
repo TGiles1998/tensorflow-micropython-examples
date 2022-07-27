6bc9173094b0e8abadc39b66ae4ebff154248955
MATTYGILO <34808802+MATTYGILO@users.noreply.github.com>
2022-07-27 16:50:21 +0100

    Update micropython make files

--- micropython-modules/cexample/micropython.cmake
+++ micropython-modules/cexample/micropython.cmake
@@ -3,13 +3,13 @@ add_library(usermod_cexample INTERFACE)
 
 # Add our source files to the lib
 target_sources(usermod_cexample INTERFACE
-        examplemodule.c
-)
+        ${CMAKE_CURRENT_LIST_DIR}/examplemodule.c
+        )
 
 # Add the current directory as an include directory.
 target_include_directories(usermod_cexample INTERFACE
-        .
-)
+        ${CMAKE_CURRENT_LIST_DIR}
+        )
 
 # Link our INTERFACE library to the usermod target.
 target_link_libraries(usermod INTERFACE usermod_cexample)
