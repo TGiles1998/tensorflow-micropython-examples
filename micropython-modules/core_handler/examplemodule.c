abed79552bae5656a99f6fc02ec90d526119af03
MATTYGILO <34808802+MATTYGILO@users.noreply.github.com>
2022-07-29 19:16:05 +0100

    Customisable function

--- micropython-modules/core_handler/examplemodule.c
+++ micropython-modules/core_handler/examplemodule.c
@@ -8,6 +8,12 @@
 
 //#define portTICK_PERIOD_MS = 10;
 
+//#define MP_THREAD_MIN_STACK_SIZE                        (4 * 1024)
+//#define MP_THREAD_DEFAULT_STACK_SIZE                    (MP_THREAD_MIN_STACK_SIZE + 1024)
+
+STATIC size_t thread_stack_size = 0;
+
+
 // This is the function which will be called from Python as cexample.add_ints(a, b).
 STATIC mp_obj_t example_add_ints(mp_obj_t a_obj, mp_obj_t b_obj) {
     // Extract the ints from the micropython input objects.
@@ -19,16 +25,33 @@ STATIC mp_obj_t example_add_ints(mp_obj_t a_obj, mp_obj_t b_obj) {
     // Calculate the addition and convert to MicroPython object.
     return mp_obj_new_int(a + b);
 }
-void task1code( void * pvParameters ) {
-    for(;;){
-        mp_printf(MICROPY_ERROR_PRINTER, "Task1code is cool core 1 \n");
-        vTaskDelay(500 / 10);
-    }
-}
-void task2code( void * pvParameters ) {
-    for(;;){
-        mp_printf(MICROPY_ERROR_PRINTER, "Task2code is cool core 0 \n");
-        vTaskDelay(700 / 10);
+//void task1code( void * pvParameters ) {
+//    for(;;){
+//        mp_printf(MICROPY_ERROR_PRINTER, "\n Task1code is cool core 1 \n");
+//        vTaskDelay(500 / 10);
+//    }
+//}
+//void task2code( void * pvParameters ) {
+//    for(;;){
+//        mp_printf(MICROPY_ERROR_PRINTER, "\n Task2code is cool core 0 \n");
+//        vTaskDelay(700 / 10);
+//    }
+//}
+
+typedef struct _core_thread_entry_args_t {
+    mp_obj_dict_t *dict_locals;
+    mp_obj_dict_t *dict_globals;
+    size_t stack_size;
+    mp_obj_t fun;
+    size_t n_args;
+    size_t n_kw;
+    mp_obj_t args[];
+} core_thread_entry_args_t;
+
+void run_function(void *args) {
+    if (nlr_push(&nlr) == 0) {
+        mp_call_function_n_kw(args->fun, args->n_args, args->n_kw, args->args);
+        nlr_pop();
     }
 }
 
@@ -37,33 +60,37 @@ STATIC mp_obj_t mod_core_function(size_t n_args, const mp_obj_t *args) {
     // We copy all arguments into this structure to keep ownership of them.
     // We must be very careful about root pointers because this pointer may
     // disappear from our address space before the thread is created.
-//    thread_entry_args_t *th_args;
-//
-//    // get positional arguments
-//    size_t pos_args_len;
-//    mp_obj_t *pos_args_items;
-//    mp_obj_get_array(args[1], &pos_args_len, &pos_args_items);
-//
-//    // just position arguments
-//    th_args = m_new_obj_var(thread_entry_args_t, mp_obj_t, pos_args_len);
-//    th_args->n_kw = 0;
-//
-//    // copy across the positional arguments
-//    th_args->n_args = pos_args_len;
-//    memcpy(th_args->args, pos_args_items, pos_args_len * sizeof(mp_obj_t));
-//
-//    // pass our locals and globals into the new thread
-//    th_args->dict_locals = mp_locals_get();
-//    th_args->dict_globals = mp_globals_get();
-//
-//    // set the stack size to use
-//    th_args->stack_size = thread_stack_size;
-//
-//    // set the function for thread entry
-//    th_args->fun = args[0];
-    
+
+    // get positional arguments
+    size_t pos_args_len;
+    mp_obj_t *pos_args_items;
+    mp_obj_get_array(args[1], &pos_args_len, &pos_args_items);
+
+    // just position arguments
+    th_args = m_new_obj_var(core_thread_entry_args_t, mp_obj_t, pos_args_len);
+    th_args->n_kw = 0;
+
+    // copy across the positional arguments
+    th_args->n_args = pos_args_len;
+    memcpy(th_args->args, pos_args_items, pos_args_len * sizeof(mp_obj_t));
+
+    // pass our locals and globals into the new thread
+    th_args->dict_locals = mp_locals_get();
+    th_args->dict_globals = mp_globals_get();
+
+
+    // set the function for thread entry
+    th_args->fun = args[0];
+
     // Gets the core id
-//    int core_id = mp_obj_get_int(args[2]);
+    char name = mp_obj_get_type_str(args[2]); //Task1
+    mp_printf(MICROPY_ERROR_PRINTER, "\n Task1code is cool core 1 \n");
+    int stack_size = mp_obj_get_int(args[3]);  // 10000
+    int priority = mp_obj_get_int(args[4]);  // 1
+    int core_id = mp_obj_get_int(args[5]);  // 1
+
+    // set the stack size to use
+    th_args->stack_size = stack_size;
 
 //    BaseType_t result = xTaskCreatePinnedToCore(
 //        freertos_entry,
@@ -76,12 +103,13 @@ STATIC mp_obj_t mod_core_function(size_t n_args, const mp_obj_t *args) {
 //    );
 //    mp_printf(MICROPY_ERROR_PRINTER, n_args);
 //    mp_printf(MICROPY_ERROR_PRINTER, args);
-
     TaskHandle_t Task1;
-    TaskHandle_t Task2;
 
-    xTaskCreatePinnedToCore(task1code, "Task1", 10000, NULL, 1, &Task1, 1);
-    xTaskCreatePinnedToCore(task2code, "Task2", 10000, NULL, 1, &Task2, 0);
+    core_thread_entry_args_t *in_args = (core_thread_entry_args_t *)th_args;
+
+    mp_printf(MICROPY_ERROR_PRINTER, "\n Starting task \n");
+    xTaskCreatePinnedToCore(run_function, name, stack_size, in_args, priority, &Task1, core_id);
+//    xTaskCreatePinnedToCore(task2code, "Task2", 10000, NULL, 1, &Task2, 0);
 
     return mp_const_none;
 }
