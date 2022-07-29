350f10acf0eeed2a0c7591f110a7eb732ecae81c
MATTYGILO <34808802+MATTYGILO@users.noreply.github.com>
2022-07-29 17:17:48 +0100

    Changed name for globals

--- micropython-modules/core_handler/examplemodule.c
+++ micropython-modules/core_handler/examplemodule.c
@@ -103,10 +103,10 @@ STATIC const mp_rom_map_elem_t example_module_globals_table[] = {
 STATIC MP_DEFINE_CONST_DICT(example_module_globals, example_module_globals_table);
 
 // Define module object.
-const mp_obj_module_t example_user_cmodule = {
+const mp_obj_module_t core_handler = {
         .base = { &mp_type_module },
         .globals = (mp_obj_dict_t *)&example_module_globals,
 };
 
 // Register the module to make it available in Python.
-MP_REGISTER_MODULE(MP_QSTR_core_handler, example_user_cmodule, 1);
+MP_REGISTER_MODULE(MP_QSTR_core_handler, core_handler, 1);
