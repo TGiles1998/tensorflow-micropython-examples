c93edf0327ffa8b1b208932cdbaa22a812814e11
MATTYGILO <34808802+MATTYGILO@users.noreply.github.com>
2022-08-03 17:56:23 +0100

    Initial lets see the problems

--- micropython-modules/wpa/enterprise.c
+++ micropython-modules/wpa/enterprise.c
@@ -10,6 +10,16 @@
 #include "esp_wpa2.h"
 #include "esp_err.h"
 
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+
 esp_err_t driver_wpa2enterprise_init() { return ESP_OK; }
 
 #define WIFI_SORT_ERRCHECK(err) do {int res = (err); if(res) {printf("Enterprise WiFi connection error: %s\n", esp_err_to_name(res)); } } while(0)
@@ -25,39 +35,78 @@ STATIC mp_obj_t mod_wpa2enterprise_connect (mp_uint_t n_args, const mp_obj_t *ar
     const char* aPassword = mp_obj_str_get_str(args[3]);
     mp_printf(MICROPY_ERROR_PRINTER, aPassword);
     int phase2 = mp_obj_get_int(args[4]);
-    wifi_config_t wifi_config = {0};
-    mp_printf(MICROPY_ERROR_PRINTER, "\n wifi_config \n");
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wstringop-truncation"
-    strncpy((char*) wifi_config.sta.ssid, aSsid, 32);
-    mp_printf(MICROPY_ERROR_PRINTER, "\n strncpy \n");
-    #pragma GCC diagnostic pop
-    WIFI_SORT_ERRCHECK(esp_wifi_stop());
-    mp_printf(MICROPY_ERROR_PRINTER, "\n esp_wifi_stop \n");
-    WIFI_SORT_ERRCHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    mp_printf(MICROPY_ERROR_PRINTER, "\n esp_wifi_set_mode \n");
-    WIFI_SORT_ERRCHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-    mp_printf(MICROPY_ERROR_PRINTER, "\n esp_wifi_set_config \n");
-    // Set WPA2 ENT config.
-    WIFI_SORT_ERRCHECK(esp_wifi_sta_wpa2_ent_set_identity((const uint8_t *) aAnonIdent, strlen(aAnonIdent)));
-    mp_printf(MICROPY_ERROR_PRINTER, "\n esp_wifi_sta_wpa2_ent_set_identity \n");
-    WIFI_SORT_ERRCHECK(esp_wifi_sta_wpa2_ent_set_username((const uint8_t *) aIdent, strlen(aIdent)));
-    mp_printf(MICROPY_ERROR_PRINTER, "\n esp_wifi_sta_wpa2_ent_set_username \n");
-    WIFI_SORT_ERRCHECK(esp_wifi_sta_wpa2_ent_set_password((const uint8_t *) aPassword, strlen(aPassword)));
-    mp_printf(MICROPY_ERROR_PRINTER, "\n esp_wifi_sta_wpa2_ent_set_password \n");
-    WIFI_SORT_ERRCHECK(esp_wifi_sta_wpa2_ent_set_ttls_phase2_method(phase2));
-    mp_printf(MICROPY_ERROR_PRINTER, "\n esp_wifi_sta_wpa2_ent_set_ttls_phase2_method \n");
-    // Enable enterprise auth.
-    WIFI_SORT_ERRCHECK(esp_wifi_sta_wpa2_ent_enable());
-    mp_printf(MICROPY_ERROR_PRINTER, "\n esp_wifi_sta_wpa2_ent_enable \n");
-    // Disable 11b as NOC asked.
-    WIFI_SORT_ERRCHECK(esp_wifi_config_11b_rate(WIFI_IF_STA, true));
-    mp_printf(MICROPY_ERROR_PRINTER, "\n esp_wifi_config_11b_rate \n");
-    // Start the connection.
-    WIFI_SORT_ERRCHECK(esp_wifi_start());
-    mp_printf(MICROPY_ERROR_PRINTER, "\n esp_wifi_start \n");
-    WIFI_SORT_ERRCHECK(esp_wifi_connect());
-    mp_printf(MICROPY_ERROR_PRINTER, "\n esp_wifi_connect \n");
+
+    #define EXAMPLE_WIFI_SSID CONFIG_EXAMPLE_WIFI_SSID
+    #define EXAMPLE_EAP_METHOD CONFIG_EXAMPLE_EAP_METHOD
+
+    #define EXAMPLE_EAP_ID CONFIG_EXAMPLE_EAP_ID
+    #define EXAMPLE_EAP_USERNAME CONFIG_EXAMPLE_EAP_USERNAME
+    #define EXAMPLE_EAP_PASSWORD CONFIG_EXAMPLE_EAP_PASSWORD
+
+
+//    #ifdef CONFIG_EXAMPLE_VALIDATE_SERVER_CERT
+//    unsigned int ca_pem_bytes = ca_pem_end - ca_pem_start;
+//    #endif /* CONFIG_EXAMPLE_VALIDATE_SERVER_CERT */
+
+//    #ifdef CONFIG_EXAMPLE_EAP_METHOD_TLS
+//    unsigned int client_crt_bytes = client_crt_end - client_crt_start;
+//        unsigned int client_key_bytes = client_key_end - client_key_start;
+//    #endif /* CONFIG_EXAMPLE_EAP_METHOD_TLS */
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL) );
+    ESP_ERROR_CHECK( esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    wifi_config_t wifi_config = {
+            .sta = {
+                    .ssid = EXAMPLE_WIFI_SSID,
+    #if defined (CONFIG_EXAMPLE_WPA3_192BIT_ENTERPRISE)
+                    .pmf_cfg = {
+                    .required = true
+                },
+    #endif
+            },
+    };
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK( esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EXAMPLE_EAP_ID, strlen(EXAMPLE_EAP_ID)) );
+
+    #if defined(CONFIG_EXAMPLE_VALIDATE_SERVER_CERT) || \
+        defined(CONFIG_EXAMPLE_WPA3_ENTERPRISE) || \
+        defined(CONFIG_EXAMPLE_WPA3_192BIT_ENTERPRISE)
+    ESP_ERROR_CHECK( esp_wifi_sta_wpa2_ent_set_ca_cert(ca_pem_start, ca_pem_bytes) );
+    #endif /* CONFIG_EXAMPLE_VALIDATE_SERVER_CERT */ /* EXAMPLE_WPA3_ENTERPRISE */
+
+    #ifdef CONFIG_EXAMPLE_EAP_METHOD_TLS
+    ESP_ERROR_CHECK( esp_wifi_sta_wpa2_ent_set_cert_key(client_crt_start, client_crt_bytes,\
+                client_key_start, client_key_bytes, NULL, 0) );
+    #endif /* CONFIG_EXAMPLE_EAP_METHOD_TLS */
+
+    #if defined CONFIG_EXAMPLE_EAP_METHOD_PEAP || CONFIG_EXAMPLE_EAP_METHOD_TTLS
+    ESP_ERROR_CHECK( esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EXAMPLE_EAP_USERNAME, strlen(EXAMPLE_EAP_USERNAME)) );
+        ESP_ERROR_CHECK( esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EXAMPLE_EAP_PASSWORD, strlen(EXAMPLE_EAP_PASSWORD)) );
+    #endif /* CONFIG_EXAMPLE_EAP_METHOD_PEAP || CONFIG_EXAMPLE_EAP_METHOD_TTLS */
+
+    #if defined CONFIG_EXAMPLE_EAP_METHOD_TTLS
+    ESP_ERROR_CHECK( esp_wifi_sta_wpa2_ent_set_ttls_phase2_method(TTLS_PHASE2_METHOD) );
+    #endif /* CONFIG_EXAMPLE_EAP_METHOD_TTLS */
+    #if defined (CONFIG_EXAMPLE_WPA3_192BIT_ENTERPRISE)
+    ESP_LOGI(TAG, "Enabling 192 bit certification");
+        ESP_ERROR_CHECK(esp_wifi_sta_wpa2_set_suiteb_192bit_certification(true));
+    #endif
+    #ifdef CONFIG_EXAMPLE_USE_DEFAULT_CERT_BUNDLE
+    ESP_ERROR_CHECK(esp_wifi_sta_wpa2_use_default_cert_bundle(true));
+    #endif
+    ESP_ERROR_CHECK( esp_wifi_sta_wpa2_ent_enable() );
+    ESP_ERROR_CHECK( esp_wifi_start() );
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mod_wpa2enterprise_connect_obj, 5, 5, mod_wpa2enterprise_connect);
