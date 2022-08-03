42e537237b812bf005f861d4978752444c59a2d2
MATTYGILO <34808802+MATTYGILO@users.noreply.github.com>
2022-08-03 14:14:00 +0100

    Correct referencing hopefully

--- micropython-modules/wpa/enterprise.c
+++ micropython-modules/wpa/enterprise.c
@@ -16,15 +16,15 @@ esp_err_t driver_wpa2enterprise_init() { return ESP_OK; }
 
 //------------------------------------------------------------------------
 STATIC mp_obj_t mod_wpa2enterprise_connect (mp_uint_t n_args, const mp_obj_t *args) {
-    const char* aSsid = mp_obj_str_get_str(args[0]); // SSID
-    const char* aAnonIdent = mp_obj_str_get_str(args[1]); // Anonoymous identidity
-    const char* aIdent = mp_obj_str_get_str(args[2]); // Identity
-    const char* aPassword = mp_obj_str_get_str(args[3]); // Password
-    int phase2 = mp_obj_get_int(args[4]); // Phase 2 Use value 1 for ESP_EAP_TTLS_PHASE2_MSCHAPV2strncpy
+    const char* aSsid = mp_obj_str_get_str(args[0]);
+    const char* aAnonIdent = mp_obj_str_get_str(args[1]);
+    const char* aIdent = mp_obj_str_get_str(args[2]);
+    const char* aPassword = mp_obj_str_get_str(args[3]);
+    int phase2 = mp_obj_get_int(args[4]);
     wifi_config_t wifi_config = {0};
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wstringop-truncation"
-    strncpy((char*) wifi_config.sta.ssid, aSsid, 32); //
+    strncpy((char*) wifi_config.sta.ssid, aSsid, 32);
     #pragma GCC diagnostic pop
     WIFI_SORT_ERRCHECK(esp_wifi_stop());
     WIFI_SORT_ERRCHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
@@ -42,38 +42,23 @@ STATIC mp_obj_t mod_wpa2enterprise_connect (mp_uint_t n_args, const mp_obj_t *ar
     WIFI_SORT_ERRCHECK(esp_wifi_start());
     WIFI_SORT_ERRCHECK(esp_wifi_connect());
 
-    //  switch (type) {
-//        case ESP_EAP_TTLS_PHASE2_EAP:
-//            g_wpa_ttls_phase2_type = "auth=EAP";
-//            break;
-//        case ESP_EAP_TTLS_PHASE2_MSCHAPV2:
-//            g_wpa_ttls_phase2_type = "auth=MSCHAPV2";
-//            break;
-//        case ESP_EAP_TTLS_PHASE2_MSCHAP:
-//            g_wpa_ttls_phase2_type = "auth=MSCHAP";
-//            break;
-//        case ESP_EAP_TTLS_PHASE2_PAP:
-//            g_wpa_ttls_phase2_type = "auth=PAP";
-//            break;
-//        case ESP_EAP_TTLS_PHASE2_CHAP:
-//            g_wpa_ttls_phase2_type = "auth=CHAP";
-//            break;
-//        default:
-//            g_wpa_ttls_phase2_type = "auth=MSCHAPV2";
-//            break;
-
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mod_wpa2enterprise_connect_obj, 5, 5, mod_wpa2enterprise_connect);
 
 //=========================================================
-STATIC const mp_rom_map_elem_t wpa2enterprise_module_globals_table[] = {
-{ MP_OBJ_NEW_QSTR(MP_QSTR_connect), MP_ROM_PTR(&mod_wpa2enterprise_connect_obj) },
-};
-STATIC MP_DEFINE_CONST_DICT(wpa2enterprise_module_globals, wpa2enterprise_module_globals_table);
 
-//===================================
-const mp_obj_module_t wpa2enterprise_module = {
-        .base = {&mp_type_module},
-        .globals = (mp_obj_dict_t *)&wpa2enterprise_module_globals,
+STATIC const mp_rom_map_elem_t enterprise_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_enterprise) },
+    { MP_ROM_QSTR(MP_QSTR_set_username_pass), MP_ROM_PTR(&mod_wpa2enterprise_connect_obj) },
 };
+
+STATIC MP_DEFINE_CONST_DICT(enterprise_module_globals, enterprise_module_globals_table);
+
+// Define module object.
+const mp_obj_module_t enterprise_module = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t *)&enterprise_module_globals,
+};
+
+MP_REGISTER_MODULE(MP_QSTR_enterprise, enterprise_module);
