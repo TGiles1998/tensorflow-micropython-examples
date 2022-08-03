a421a8de3a3ee7360cd10f43a031bc435b370a70
MATTYGILO <34808802+MATTYGILO@users.noreply.github.com>
2022-08-03 20:07:06 +0100

    Printing ssid on own prior

--- micropython-modules/wpa/enterprise.c
+++ micropython-modules/wpa/enterprise.c
@@ -102,7 +102,7 @@ STATIC mp_obj_t mod_wpa2enterprise_connect (mp_uint_t n_args, const mp_obj_t *ar
 //    #endif
 //            },
 //    };
-//    mp_printf(MICROPY_ERROR_PRINTER, wifi_config.sta.ssid);
+    ESP_ERROR_CHECK( mp_printf(MICROPY_ERROR_PRINTER, wifi_config.sta.ssid) );
 
 //    mp_printf(TAG, "Setting WiFi configuration SSID %s...", *wifi_config.sta.ssid);
     mp_printf(MICROPY_ERROR_PRINTER, "\n Ok gonna try Setting WiFi configuration SSID\n");
