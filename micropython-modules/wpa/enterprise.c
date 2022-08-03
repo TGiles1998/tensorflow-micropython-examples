368cf0ec3d70e66eb635ee780b1d61f8e78b7051
MATTYGILO <34808802+MATTYGILO@users.noreply.github.com>
2022-08-03 20:16:57 +0100

    Printing pointers wifi_config.sta.ssid

--- micropython-modules/wpa/enterprise.c
+++ micropython-modules/wpa/enterprise.c
@@ -103,7 +103,7 @@ STATIC mp_obj_t mod_wpa2enterprise_connect (mp_uint_t n_args, const mp_obj_t *ar
 //            },
 //    };
 
-    ESP_ERROR_CHECK( mp_print_str(MICROPY_ERROR_PRINTER, wifi_config.sta.ssid) );
+//    ESP_ERROR_CHECK( mp_print_str(MICROPY_ERROR_PRINTER, wifi_config.sta.ssid) );
     mp_printf(MICROPY_ERROR_PRINTER, "\n Ok gonna try Setting WiFi configuration SSID\n");
     ESP_ERROR_CHECK( mp_printf(MICROPY_ERROR_PRINTER, *wifi_config.sta.ssid) );
 //    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
